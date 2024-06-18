540204d39a3d5c9915a3cf3e1fd20b2420f9b097
radzp <anne.paczesna@gmail.com>
2024-06-18 02:56:29 +0200

    Commit end

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = ({toggleModal, nbOfContacts}) => {
+    return (
+        <header className="header">
+            <div className='container'>
+                <h3>
+                    Contact List ({nbOfContacts})
+                </h3>
+                <button onClick={() => toggleModal(true)} className='btn'>
+                    <i className='bi bi-plus-square'></i> Add new contact
+                </button>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
